f4a24f4558804778ad6521dd73ceac3e2b3ddc2b
Hippobot <malhousseiny@outlook.com>
2021-04-26 11:10:34 -0400

    Create Project3_Mahmooud_Alhousseiny.cpp
    A function that creates a String object in memory to hold the text "Computer hardware Graphics Quality Recommendation Tool"

---
+++ Project3_Mahmooud_Alhousseiny.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+void displayTitle()
+{
+	string comp = "Computer Hardware Graphics Quality Recommendation Tool";
+	cout<<"\n\n"<<comp<<"\n\n";
+	
+}
+
+
